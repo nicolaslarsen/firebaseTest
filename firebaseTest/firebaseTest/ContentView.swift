3f66101f8ff76568019cffaa046e1dfa547bac30
Nicolas Ringsmose <nic>
2020-08-11 21:34:26 +0200

    initial project

---
+++ firebaseTest/firebaseTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  firebaseTest
+//
+//  Created by Nicolas Ringsmose on 11/08/2020.
+//  Copyright © 2020 Test. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
