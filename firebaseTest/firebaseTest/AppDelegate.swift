11978c1ddd22f288fca5368498c827ab520cc5cd
Nicolas Larsen <nicolasringsmose@hotmail.com>
2020-08-12 22:40:27 +0200

    Initial firebase setup with Authentication, Firestore, and Analytics

--- firebaseTest/firebaseTest/AppDelegate.swift
+++ firebaseTest/firebaseTest/AppDelegate.swift
@@ -7,13 +7,17 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // Use Firebase library to configure APIs
+        FirebaseApp.configure()
+        
         // Override point for customization after application launch.
         return true
     }
